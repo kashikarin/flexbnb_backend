a6b195f9d9174eaf1f1f3437e323328ca7634fd1
Idan Bahar <bahar.idan91@gmail.com>
2025-09-08 21:29:32 +0300

    some minor changes

--- api/home/home.routes.js
+++ api/home/home.routes.js
@@ -1,24 +1,29 @@
-    import express from 'express'
-    import {
-    addHome,
-    getHome,
-    getHomes,
-    updateHome,
-    getHealth,
-    } from './home.controller.js'
+import express from 'express'
+import {
+  addHome,
+  getHome,
+  getHomes,
+  updateHome,
+  getHealth,
+} from './home.controller.js'
 
-    const router = express.Router()
+const router = express.Router()
+// @route   GET /api/homes
+// @desc    Health check
+// @access Public
+router.get('/health', getHealth)
 
-    router.get('/health', getHealth)
-    // @route  GET /api/homes
-    // @desc   Get all homes
-    // @access Public
-    router.get('/', getHomes)
-    router.get('/:homeId', getHome)
-    router.post('/', addHome)
-    router.put('/:homeId', updateHome)
-    // @route  POST /api/homes
-    // @desc   Add a new home
-    // @access Authenticated
+// @route  GET /api/homes
+// @desc   Get all homes
+// @access Public
+router.get('/', getHomes)
 
-    export const homeRoutes = router
+router.get('/:homeId', getHome)
+router.put('/:homeId', updateHome)
+
+// @route  POST /api/homes
+// @desc   Add a new home
+// @access Authenticated
+router.post('/', addHome)
+
+export const homeRoutes = router
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
     origin: 'http://localhost:5173',
+    origin: 'http://localhost:5174',
     credentials: true,
   })
 )
