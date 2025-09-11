920c74c076a341bead6acb41ebc673b994ae5c8c
kashikarin <karinkashi@gmail.com>
2025-09-11 11:37:54 +0300

    fix auth midlewhere path at cloudinary routes file

--- api/cloudinary/cloudinary.routes.js
+++ api/cloudinary/cloudinary.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { getSignature } from "./cloudinary.controller.js"
-import { requireAuth } from "../../middlewares/requireAuth.js"
+import { requireAuth } from "../../middleware/auth.middleware.js"
 
 const router = express.Router()
 
