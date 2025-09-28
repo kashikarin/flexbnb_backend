9900e127071b923eb319085b78920382ce248c9e
Idan Bahar <bahar.idan91@gmail.com>
2025-09-28 18:45:46 +0300

    fixing auth google

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ if (process.env.NODE_ENV === 'production') {
       'http://localhost:5173',
       'http://127.0.0.1:5174',
       'http://localhost:5174',
+      'https://flexbnb.onrender.com',
     ],
     credentials: true,
   }
